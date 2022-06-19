66642099137dd65d2e34e6fd11ade3c4ac02e186
Rajesh25B <basvoju.rajesh@gmail.com>
2022-06-20 00:40:33 +0530

    Implement Token based authorization using DRF

--- app/user/views.py
+++ app/user/views.py
@@ -1,8 +1,3 @@
-from django.conf import settings
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-from rest_framework.authtoken.models import Token
-
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework import generics, authentication, permissions
 from rest_framework.settings import api_settings
@@ -13,12 +8,6 @@ from user.serializers import UserSerializer, AuthTokenSerializer
 class CreateUserView(generics.CreateAPIView):
     serializer_class = UserSerializer
     
-    @receiver(post_save, sender=settings.AUTH_USER_MODEL)
-    def create_auth_token(sender, instance=None, created=False, **kwargs):
-        if created:
-            Token.objects.create(user=instance)
-    
-
 class CustomAuthToken(ObtainAuthToken):
     serializer_class = AuthTokenSerializer
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
